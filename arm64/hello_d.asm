9cadc7adba71b89566f3bdb71492942b4cf3d1bf
Allen Guo <guosonglin001@gmail.com>
2023-11-02 13:52:56 +0800

    init

---
+++ arm64/hello_d.asm
@@ -0,0 +1,317 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.build_version macos, 14, 0	sdk_version 14, 0
+	.file	1 "/Users/allen/repos/asm_demos" "src/hello.c"
+	.globl	_main                           ; -- Begin function main
+	.p2align	2
+_main:                                  ; @main
+Lfunc_begin0:
+	.loc	1 3 0                           ; src/hello.c:3:0
+	.cfi_startproc
+; %bb.0:
+	sub	sp, sp, #32
+	.cfi_def_cfa_offset 32
+	stp	x29, x30, [sp, #16]             ; 16-byte Folded Spill
+	add	x29, sp, #16
+	.cfi_def_cfa w29, 16
+	.cfi_offset w30, -8
+	.cfi_offset w29, -16
+	mov	w8, #0
+	str	w8, [sp, #8]                    ; 4-byte Folded Spill
+	stur	wzr, [x29, #-4]
+	adrp	x0, l_.str@PAGE
+	add	x0, x0, l_.str@PAGEOFF
+Ltmp0:
+	.loc	1 4 3 prologue_end              ; src/hello.c:4:3
+	bl	_printf
+	ldr	w0, [sp, #8]                    ; 4-byte Folded Reload
+	.loc	1 5 3                           ; src/hello.c:5:3
+	ldp	x29, x30, [sp, #16]             ; 16-byte Folded Reload
+	add	sp, sp, #32
+	ret
+Ltmp1:
+Lfunc_end0:
+	.cfi_endproc
+                                        ; -- End function
+	.section	__TEXT,__cstring,cstring_literals
+l_.str:                                 ; @.str
+	.asciz	"hello\n"
+
+	.section	__DWARF,__debug_abbrev,regular,debug
+Lsection_abbrev:
+	.byte	1                               ; Abbreviation Code
+	.byte	17                              ; DW_TAG_compile_unit
+	.byte	1                               ; DW_CHILDREN_yes
+	.byte	37                              ; DW_AT_producer
+	.byte	14                              ; DW_FORM_strp
+	.byte	19                              ; DW_AT_language
+	.byte	5                               ; DW_FORM_data2
+	.byte	3                               ; DW_AT_name
+	.byte	14                              ; DW_FORM_strp
+	.ascii	"\202|"                         ; DW_AT_LLVM_sysroot
+	.byte	14                              ; DW_FORM_strp
+	.ascii	"\357\177"                      ; DW_AT_APPLE_sdk
+	.byte	14                              ; DW_FORM_strp
+	.byte	16                              ; DW_AT_stmt_list
+	.byte	23                              ; DW_FORM_sec_offset
+	.byte	27                              ; DW_AT_comp_dir
+	.byte	14                              ; DW_FORM_strp
+	.byte	17                              ; DW_AT_low_pc
+	.byte	1                               ; DW_FORM_addr
+	.byte	18                              ; DW_AT_high_pc
+	.byte	6                               ; DW_FORM_data4
+	.byte	0                               ; EOM(1)
+	.byte	0                               ; EOM(2)
+	.byte	2                               ; Abbreviation Code
+	.byte	52                              ; DW_TAG_variable
+	.byte	0                               ; DW_CHILDREN_no
+	.byte	73                              ; DW_AT_type
+	.byte	19                              ; DW_FORM_ref4
+	.byte	58                              ; DW_AT_decl_file
+	.byte	11                              ; DW_FORM_data1
+	.byte	59                              ; DW_AT_decl_line
+	.byte	11                              ; DW_FORM_data1
+	.byte	2                               ; DW_AT_location
+	.byte	24                              ; DW_FORM_exprloc
+	.byte	0                               ; EOM(1)
+	.byte	0                               ; EOM(2)
+	.byte	3                               ; Abbreviation Code
+	.byte	1                               ; DW_TAG_array_type
+	.byte	1                               ; DW_CHILDREN_yes
+	.byte	73                              ; DW_AT_type
+	.byte	19                              ; DW_FORM_ref4
+	.byte	0                               ; EOM(1)
+	.byte	0                               ; EOM(2)
+	.byte	4                               ; Abbreviation Code
+	.byte	33                              ; DW_TAG_subrange_type
+	.byte	0                               ; DW_CHILDREN_no
+	.byte	73                              ; DW_AT_type
+	.byte	19                              ; DW_FORM_ref4
+	.byte	55                              ; DW_AT_count
+	.byte	11                              ; DW_FORM_data1
+	.byte	0                               ; EOM(1)
+	.byte	0                               ; EOM(2)
+	.byte	5                               ; Abbreviation Code
+	.byte	36                              ; DW_TAG_base_type
+	.byte	0                               ; DW_CHILDREN_no
+	.byte	3                               ; DW_AT_name
+	.byte	14                              ; DW_FORM_strp
+	.byte	62                              ; DW_AT_encoding
+	.byte	11                              ; DW_FORM_data1
+	.byte	11                              ; DW_AT_byte_size
+	.byte	11                              ; DW_FORM_data1
+	.byte	0                               ; EOM(1)
+	.byte	0                               ; EOM(2)
+	.byte	6                               ; Abbreviation Code
+	.byte	36                              ; DW_TAG_base_type
+	.byte	0                               ; DW_CHILDREN_no
+	.byte	3                               ; DW_AT_name
+	.byte	14                              ; DW_FORM_strp
+	.byte	11                              ; DW_AT_byte_size
+	.byte	11                              ; DW_FORM_data1
+	.byte	62                              ; DW_AT_encoding
+	.byte	11                              ; DW_FORM_data1
+	.byte	0                               ; EOM(1)
+	.byte	0                               ; EOM(2)
+	.byte	7                               ; Abbreviation Code
+	.byte	46                              ; DW_TAG_subprogram
+	.byte	0                               ; DW_CHILDREN_no
+	.byte	17                              ; DW_AT_low_pc
+	.byte	1                               ; DW_FORM_addr
+	.byte	18                              ; DW_AT_high_pc
+	.byte	6                               ; DW_FORM_data4
+	.byte	64                              ; DW_AT_frame_base
+	.byte	24                              ; DW_FORM_exprloc
+	.byte	3                               ; DW_AT_name
+	.byte	14                              ; DW_FORM_strp
+	.byte	58                              ; DW_AT_decl_file
+	.byte	11                              ; DW_FORM_data1
+	.byte	59                              ; DW_AT_decl_line
+	.byte	11                              ; DW_FORM_data1
+	.byte	73                              ; DW_AT_type
+	.byte	19                              ; DW_FORM_ref4
+	.byte	63                              ; DW_AT_external
+	.byte	25                              ; DW_FORM_flag_present
+	.byte	0                               ; EOM(1)
+	.byte	0                               ; EOM(2)
+	.byte	0                               ; EOM(3)
+	.section	__DWARF,__debug_info,regular,debug
+Lsection_info:
+Lcu_begin0:
+.set Lset0, Ldebug_info_end0-Ldebug_info_start0 ; Length of Unit
+	.long	Lset0
+Ldebug_info_start0:
+	.short	4                               ; DWARF version number
+.set Lset1, Lsection_abbrev-Lsection_abbrev ; Offset Into Abbrev. Section
+	.long	Lset1
+	.byte	8                               ; Address Size (in bytes)
+	.byte	1                               ; Abbrev [1] 0xb:0x73 DW_TAG_compile_unit
+	.long	0                               ; DW_AT_producer
+	.short	12                              ; DW_AT_language
+	.long	47                              ; DW_AT_name
+	.long	59                              ; DW_AT_LLVM_sysroot
+	.long	111                             ; DW_AT_APPLE_sdk
+.set Lset2, Lline_table_start0-Lsection_line ; DW_AT_stmt_list
+	.long	Lset2
+	.long	122                             ; DW_AT_comp_dir
+	.quad	Lfunc_begin0                    ; DW_AT_low_pc
+.set Lset3, Lfunc_end0-Lfunc_begin0     ; DW_AT_high_pc
+	.long	Lset3
+	.byte	2                               ; Abbrev [2] 0x32:0x11 DW_TAG_variable
+	.long	67                              ; DW_AT_type
+	.byte	1                               ; DW_AT_decl_file
+	.byte	4                               ; DW_AT_decl_line
+	.byte	9                               ; DW_AT_location
+	.byte	3
+	.quad	l_.str
+	.byte	3                               ; Abbrev [3] 0x43:0xc DW_TAG_array_type
+	.long	79                              ; DW_AT_type
+	.byte	4                               ; Abbrev [4] 0x48:0x6 DW_TAG_subrange_type
+	.long	86                              ; DW_AT_type
+	.byte	7                               ; DW_AT_count
+	.byte	0                               ; End Of Children Mark
+	.byte	5                               ; Abbrev [5] 0x4f:0x7 DW_TAG_base_type
+	.long	151                             ; DW_AT_name
+	.byte	6                               ; DW_AT_encoding
+	.byte	1                               ; DW_AT_byte_size
+	.byte	6                               ; Abbrev [6] 0x56:0x7 DW_TAG_base_type
+	.long	156                             ; DW_AT_name
+	.byte	8                               ; DW_AT_byte_size
+	.byte	7                               ; DW_AT_encoding
+	.byte	7                               ; Abbrev [7] 0x5d:0x19 DW_TAG_subprogram
+	.quad	Lfunc_begin0                    ; DW_AT_low_pc
+.set Lset4, Lfunc_end0-Lfunc_begin0     ; DW_AT_high_pc
+	.long	Lset4
+	.byte	1                               ; DW_AT_frame_base
+	.byte	109
+	.long	177                             ; DW_AT_name
+	.byte	1                               ; DW_AT_decl_file
+	.byte	3                               ; DW_AT_decl_line
+	.long	118                             ; DW_AT_type
+                                        ; DW_AT_external
+	.byte	5                               ; Abbrev [5] 0x76:0x7 DW_TAG_base_type
+	.long	182                             ; DW_AT_name
+	.byte	5                               ; DW_AT_encoding
+	.byte	4                               ; DW_AT_byte_size
+	.byte	0                               ; End Of Children Mark
+Ldebug_info_end0:
+	.section	__DWARF,__debug_str,regular,debug
+Linfo_string:
+	.asciz	"Apple clang version 15.0.0 (clang-1500.0.40.1)" ; string offset=0
+	.asciz	"src/hello.c"                   ; string offset=47
+	.asciz	"/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk" ; string offset=59
+	.asciz	"MacOSX.sdk"                    ; string offset=111
+	.asciz	"/Users/allen/repos/asm_demos"  ; string offset=122
+	.asciz	"char"                          ; string offset=151
+	.asciz	"__ARRAY_SIZE_TYPE__"           ; string offset=156
+	.byte	0                               ; string offset=176
+	.asciz	"main"                          ; string offset=177
+	.asciz	"int"                           ; string offset=182
+	.section	__DWARF,__apple_names,regular,debug
+Lnames_begin:
+	.long	1212240712                      ; Header Magic
+	.short	1                               ; Header Version
+	.short	0                               ; Header Hash Function
+	.long	2                               ; Header Bucket Count
+	.long	2                               ; Header Hash Count
+	.long	12                              ; Header Data Length
+	.long	0                               ; HeaderData Die Offset Base
+	.long	1                               ; HeaderData Atom Count
+	.short	1                               ; DW_ATOM_die_offset
+	.short	6                               ; DW_FORM_data4
+	.long	0                               ; Bucket 0
+	.long	1                               ; Bucket 1
+	.long	2090499946                      ; Hash in Bucket 0
+	.long	5381                            ; Hash in Bucket 1
+.set Lset5, LNames1-Lnames_begin        ; Offset in Bucket 0
+	.long	Lset5
+.set Lset6, LNames0-Lnames_begin        ; Offset in Bucket 1
+	.long	Lset6
+LNames1:
+	.long	177                             ; main
+	.long	1                               ; Num DIEs
+	.long	93
+	.long	0
+LNames0:
+	.long	176                             ; 
+	.long	1                               ; Num DIEs
+	.long	50
+	.long	0
+	.section	__DWARF,__apple_objc,regular,debug
+Lobjc_begin:
+	.long	1212240712                      ; Header Magic
+	.short	1                               ; Header Version
+	.short	0                               ; Header Hash Function
+	.long	1                               ; Header Bucket Count
+	.long	0                               ; Header Hash Count
+	.long	12                              ; Header Data Length
+	.long	0                               ; HeaderData Die Offset Base
+	.long	1                               ; HeaderData Atom Count
+	.short	1                               ; DW_ATOM_die_offset
+	.short	6                               ; DW_FORM_data4
+	.long	-1                              ; Bucket 0
+	.section	__DWARF,__apple_namespac,regular,debug
+Lnamespac_begin:
+	.long	1212240712                      ; Header Magic
+	.short	1                               ; Header Version
+	.short	0                               ; Header Hash Function
+	.long	1                               ; Header Bucket Count
+	.long	0                               ; Header Hash Count
+	.long	12                              ; Header Data Length
+	.long	0                               ; HeaderData Die Offset Base
+	.long	1                               ; HeaderData Atom Count
+	.short	1                               ; DW_ATOM_die_offset
+	.short	6                               ; DW_FORM_data4
+	.long	-1                              ; Bucket 0
+	.section	__DWARF,__apple_types,regular,debug
+Ltypes_begin:
+	.long	1212240712                      ; Header Magic
+	.short	1                               ; Header Version
+	.short	0                               ; Header Hash Function
+	.long	3                               ; Header Bucket Count
+	.long	3                               ; Header Hash Count
+	.long	20                              ; Header Data Length
+	.long	0                               ; HeaderData Die Offset Base
+	.long	3                               ; HeaderData Atom Count
+	.short	1                               ; DW_ATOM_die_offset
+	.short	6                               ; DW_FORM_data4
+	.short	3                               ; DW_ATOM_die_tag
+	.short	5                               ; DW_FORM_data2
+	.short	4                               ; DW_ATOM_type_flags
+	.short	11                              ; DW_FORM_data1
+	.long	-1                              ; Bucket 0
+	.long	-1                              ; Bucket 1
+	.long	0                               ; Bucket 2
+	.long	193495088                       ; Hash in Bucket 2
+	.long	2090147939                      ; Hash in Bucket 2
+	.long	-594775205                      ; Hash in Bucket 2
+.set Lset7, Ltypes1-Ltypes_begin        ; Offset in Bucket 2
+	.long	Lset7
+.set Lset8, Ltypes2-Ltypes_begin        ; Offset in Bucket 2
+	.long	Lset8
+.set Lset9, Ltypes0-Ltypes_begin        ; Offset in Bucket 2
+	.long	Lset9
+Ltypes1:
+	.long	182                             ; int
+	.long	1                               ; Num DIEs
+	.long	118
+	.short	36
+	.byte	0
+	.long	0
+Ltypes2:
+	.long	151                             ; char
+	.long	1                               ; Num DIEs
+	.long	79
+	.short	36
+	.byte	0
+	.long	0
+Ltypes0:
+	.long	156                             ; __ARRAY_SIZE_TYPE__
+	.long	1                               ; Num DIEs
+	.long	86
+	.short	36
+	.byte	0
+	.long	0
+.subsections_via_symbols
+	.section	__DWARF,__debug_line,regular,debug
+Lsection_line:
+Lline_table_start0:
---
+++ src/mac.sh
@@ -0,0 +1,2 @@
+clang -g -S src/hello.c -o arm64/hello_d.asm
+clang -S src/hello.c -o arm64/hello.asm
