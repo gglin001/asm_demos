ec28a1e02016d322a02ee105a3e84088bd1d5939
Allen Guo <guosonglin001@gmail.com>
2023-11-06 14:54:25 +0800

    wip

--- src/temp.c
+++ src/temp.c
@@ -4,11 +4,11 @@ void func() {
   int a = 10;
   int b = 110;
   int c = a + b;
-  printf("c: %i", c);
+  // printf("c: %i", c);
 }
 
 int main() {
-  printf("hello\n");
+  // printf("hello\n");
   func();
   return 0;
 }
