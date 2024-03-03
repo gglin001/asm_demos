d5b08b50f16dcc7a854b0d0025bd991f31545c84
Allen Guo <guosonglin001@gmail.com>
2024-03-03 22:59:43 +0800

    wip

--- debug.sh
+++ debug.sh
@@ -8,10 +8,13 @@ spike --isa rv64gcv -d pk a.out
 
 # in qemu docker container
 qemu-riscv64 \
-    rv64,v=true,vext_spec=v1.0 \
-    -g 1234 a.out
+  -cpu rv64,v=true,vext_spec=v1.0 a.out
 
-# # TODO support gdb on mac?
+# TODO support gdb on mac?
+# # in qemu docker container
+# qemu-riscv64 \
+#   rv64,v=true,vext_spec=v1.0 \
+#   -g 1234 a.out
 # # host
 # riscv64-unknown-elf-gdb a.out
 # # target remote :1234
