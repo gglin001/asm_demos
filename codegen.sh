15602f3a3625a6cef3f65d8c58a3c3e39cd5a925
Allen Guo <guosonglin001@gmail.com>
2024-03-03 22:03:33 +0800

    wip

--- codegen.sh
+++ codegen.sh
@@ -5,5 +5,6 @@ riscv64-unknown-elf-gcc -S -O0 src/hello.c -o rv64/hello.asm
 riscv64-unknown-elf-gcc -S -g -O0 src/hello.c -o rv64/hello_d.asm
 
 # arm macos
-clang -S src/hello.c -o arm64/hello.asm
-clang -g -S src/hello.c -o arm64/hello_d.asm
+# https://clang.llvm.org/docs/CrossCompilation.html#target-triple
+clang -target arm64-apple-darwin -S -O0 src/hello.c -o arm64/hello.asm
+clang -target arm64-apple-darwin -S -g -O0 src/hello.c -o arm64/hello_d.asm
--- rv64/hello.asm
+++ rv64/hello.asm
@@ -13,30 +13,18 @@
 	.globl	main
 	.type	main, @function
 main:
-.LFB1:
-	.cfi_startproc
 	addi	sp,sp,-16
-	.cfi_def_cfa_offset 16
 	sd	ra,8(sp)
 	sd	s0,0(sp)
-	.cfi_offset 1, -8
-	.cfi_offset 8, -16
 	addi	s0,sp,16
-	.cfi_def_cfa 8, 0
 	lui	a5,%hi(.LC0)
 	addi	a0,a5,%lo(.LC0)
 	call	puts
 	li	a5,0
 	mv	a0,a5
 	ld	ra,8(sp)
-	.cfi_restore 1
 	ld	s0,0(sp)
-	.cfi_restore 8
-	.cfi_def_cfa 2, 16
 	addi	sp,sp,16
-	.cfi_def_cfa_offset 0
 	jr	ra
-	.cfi_endproc
-.LFE1:
 	.size	main, .-main
 	.ident	"GCC: (gc891d8dc2) 13.2.0"
