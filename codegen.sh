db12cd686566c46c82fdacaddec34542266dfc97
Allen Guo <guosonglin001@gmail.com>
2024-03-03 22:26:48 +0800

    wip

--- codegen.sh
+++ codegen.sh
@@ -1,8 +1,13 @@
 # riscv
 # setup `toolchain/readme.md` first
-
+# gcc, which is used now
 riscv64-unknown-elf-gcc -S -O0 src/hello.c -o rv64/hello.asm
 riscv64-unknown-elf-gcc -S -g -O0 src/hello.c -o rv64/hello_d.asm
+# clang
+clang --target=riscv64-unknown-elf --gcc-toolchain=$PWD/toolchain/riscv-gnu-toolchain -I $PWD/toolchain/riscv-gnu-toolchain/riscv64-unknown-elf/include \
+  -S -O0 -o rv64/hello.asm src/hello.c
+clang --target=riscv64-unknown-elf --gcc-toolchain=$PWD/toolchain/riscv-gnu-toolchain -I $PWD/toolchain/riscv-gnu-toolchain/riscv64-unknown-elf/include \
+  -S -g -O0 -o rv64/hello_d.asm src/hello.c
 
 # arm macos
 # https://clang.llvm.org/docs/CrossCompilation.html#target-triple
