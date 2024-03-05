e4a5cd53911d776a9f71cc044599e76e42c215e1
Allen Guo <guosonglin001@gmail.com>
2024-03-05 02:32:57 +0000

    wip

--- debug.im.sh
+++ debug.im.sh
@@ -1,30 +1,50 @@
 # toolchain is built with
 # `./configure --prefix=$RISCV --with-arch=rv64im --with-abi=lp64 --with-sim=spike --enable-llvm`
 
-# hello
-riscv64-unknown-elf-gcc -g -O0 src/hello.c
-
-# rvv
+# gcc
 args=(
-  -g
-  -O0
   -march=rv64im
-  # rv64/rvv/strlen.c
-  # rv64/rvv/strlen.s
-  rv64/rvv/saxpy.c
-  rv64/rvv/saxpy.s
+  -mabi=lp64
+  # -g
+  -O0
+  # -S
+  -v
+  src/hello.c
+  # src/rvv_strlen.c
 )
 set -x
 riscv64-unknown-elf-gcc "${args[@]}"
 set +x
 
-# spike
-spike --isa rv64im pk a.out
-# spike --isa rv64gc pk a.out
+# clang
+args=(
+  -march=rv64im
+  -mabi=lp64
+  --gcc-toolchain=$PWD/toolchain/riscv-gnu-toolchain
+  --target=riscv64-unknown-elf
+  -I$PWD/toolchain/riscv-gnu-toolchain/riscv64-unknown-elf/include
+  # -g
+  -O0
+  # -S
+  -v
+  src/hello.c
+  # src/rvv_strlen.c
+)
+set -x
+clang "${args[@]}"
+set +x
 
-# spike debug
-spike --isa rv64im -d pk a.out
+# =============================================================================
+
+# spike
+spike --isa rv64gc pk a.out
+
+spike --isa rv64im pk a.out
 
 # in qemu docker container
 qemu-riscv64 \
-  -cpu rv64,v=true,vext_spec=v1.0 a.out
+  -cpu rv64 a.out
+
+# =============================================================================
+
+# debug
