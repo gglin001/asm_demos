ef29458313667d48e7520701c02ed0127cd43fba
Allen Guo <guosonglin001@gmail.com>
2024-03-05 02:22:04 +0000

    add rvv_strlen.c

--- debug.gcv.sh
+++ debug.gcv.sh
@@ -1,22 +1,40 @@
 # toolchain is built with
 # `./configure --prefix=$RISCV --with-arch=rv64gc --with-abi=lp64d --with-sim=spike --enable-llvm`
 
-# hello
-riscv64-unknown-elf-gcc -g -O0 src/hello.c
-
-# rvv
+# gcc
 args=(
-  -g
-  -O0
   -march=rv64gcv
-  # rv64/rvv/strlen.c
-  # rv64/rvv/strlen.s
-  rv64/rvv/saxpy.c
-  rv64/rvv/saxpy.s
+  -mabi=lp64d
+  # -g
+  -O0
+  # -S
+  -v
+  # src/hello.c
+  src/rvv_strlen.c
 )
 set -x
 riscv64-unknown-elf-gcc "${args[@]}"
 set +x
+
+# clang
+args=(
+  -march=rv64gcv
+  -mabi=lp64d
+  --gcc-toolchain=$PWD/toolchain/riscv-gnu-toolchain
+  --target=riscv64-unknown-elf
+  -I$PWD/toolchain/riscv-gnu-toolchain/riscv64-unknown-elf/include
+  # -g
+  -O0
+  # -S
+  -v
+  # src/hello.c
+  src/rvv_strlen.c
+)
+set -x
+clang "${args[@]}"
+set +x
+
+# =============================================================================
 
 # spike
 spike --isa rv64gcv pk a.out
@@ -27,6 +45,10 @@ spike --isa rv64gcv -d pk a.out
 # in qemu docker container
 qemu-riscv64 \
   -cpu rv64,v=true,vext_spec=v1.0 a.out
+
+# =============================================================================
+
+# debug
 
 # TODO support gdb on mac?
 # # in qemu docker container
