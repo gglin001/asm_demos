64e91a19e6d37a03a1fbd04249434fceb1e352d9
Allen Guo <guosonglin001@gmail.com>
2024-06-05 21:21:23 +0800

    wip

--- mlir/inline_asm_riscv.sh
+++ mlir/inline_asm_riscv.sh
@@ -49,6 +49,18 @@ clang "${args[@]}"
 
 DIR=_demos/inline_asm_riscv.mlir && mkdir -p $DIR
 args=(
+  # -M no-aliases
+  # -x
+  # -s
+  -d
+  $DIR/main
+)
+llvm-objdump "${args[@]}" >$DIR/main.objdump
+
+#####
+
+DIR=_demos/inline_asm_riscv.mlir && mkdir -p $DIR
+args=(
   # -l
   --isa rv64gc
   pk
