c5399e6051a891ded0e1283f1b429a0f53b2d374
Allen Guo <guosonglin001@gmail.com>
2025-11-13 17:20:16 +0800

    wip

--- collections/compile.sh
+++ collections/compile.sh
@@ -2,10 +2,18 @@ DIR=_demos/_collections && mkdir -p $DIR
 args=(
   -v
   #
-  # -S
+  -O0
+  # -O1
+  # -O2
   #
   $DIR/main.ll
-  -o $DIR/main.ll.elf
   #
+  # -o $DIR/main.ll.elf
+  #
+  -S
+  -o $DIR/main.ll.asm
+  #
+  # -mllvm -print-before-all
+  # -mllvm -print-after-all
 )
 clang "${args[@]}"
